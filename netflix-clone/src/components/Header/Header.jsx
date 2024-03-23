0948a8786b74c364f7111cd786d7f448f54a4daf
geremewatalay <geremewatalay@gmail.com>
2024-03-23 13:18:02 -0700

    update footer

--- netflix-clone/src/components/Header/Header.jsx
+++ netflix-clone/src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import "./header.css";
 import NetflixLogo from "../../assets/images/NetflixLogo.png"
-// import SearchIcon from '@mui/icons-material/Search';
-// import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
-// import AccountBoxIcon from '@mui/icons-material/AccountBox';
-// import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 const Header = () => {
   return (
@@ -22,14 +22,14 @@ const Header = () => {
                       <li>Browse by Languages</li>
                   </ul>
               </div>
-              {/* <div className='header_right'>
+              <div className='header_right'>
                   <ul>
                       <li><SearchIcon /></li>
                       <li><NotificationsNoneIcon /></li>
                       <li><AccountBoxIcon /></li>
                       <li><ArrowDropDownIcon /></li>
                   </ul>
-              </div> */}
+              </div>
           </div> 
     </div>
   )
